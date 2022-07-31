079888775518e72f8426f72694878e2b2e5d77c8
Sahib Singh <sahibs2@illinois.edu>
2022-07-31 22:07:26 +0530

    updated integrity constraints

--- database/farmer_market_integrity.sql
+++
@@ -1,8 +0,0 @@
--- checking that there is no duplicate farmer market reported.
-SELECT * FROM farmers_market
-WHERE FMID in (SELECT FMID from farmers_market
-              Group by FMID having count(*)>1);
-
--- checking no farmer market records has empty FMID.
-SELECT * FROM farmers_market
-WHERE FMID  = NULL;
